6e8ba94c1efc2162d1baab3cf23cbe525b7ddaf9
MarioGogogo <148373644@qq.com>
2023-06-26 19:21:14 +0800

    feat:删除不必要的参数,优化代码体积

--- rollup.config.js
+++ rollup.config.js
@@ -7,26 +7,6 @@ import { name } from './package.json';
 import { babel } from '@rollup/plugin-babel';
 export default [
   {
-    input: 'lib/test.js', // 入口文件
-    output: {
-      file: `dist/bundle.min.js`, // 输出文件
-      format: 'umd',
-      name, //这将指定在 UMD 模块中使用的全局变量名称。
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      babel({
-        exclude: 'node_modules/**', // 不编译 node_modules 中的代码
-        presets: ['@babel/preset-env'],
-      }),
-      deletePlugin({
-        targets: 'dist/*',
-      }),
-      // uglify(),
-    ],
-  },
-  {
     input: 'lib/index.js', // 入口文件
     output: {
       file: 'dist/xr-monitor-sdk.js', // 输出文件
@@ -41,9 +21,9 @@ export default [
         exclude: 'node_modules/**', // 不编译 node_modules 中的代码
         plugins: [['transform-remove-console', { exclude: ['error', 'warn'] }]], //删除所有 console.log 语句
       }),
-      // deletePlugin({
-      //   targets: 'dist/*',
-      // }),
+      deletePlugin({
+        targets: 'dist/*',
+      }),
       // js 压缩插件，需要在最后引入
       uglify(),
     ],
